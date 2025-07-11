71d253cba89eb8fa86d874509efeab4f55896c40
Tamara Romanczuk <112263180+anna-tama@users.noreply.github.com>
2025-07-11 16:59:54 -0300

    main: agrego dockerfile y mongoAtlas

--- billboard-back/config/db.js
+++ billboard-back/config/db.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/billboard";
+
+let client;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
     console.log('MongoDB Connected...');
+    return client.db(); // Retorna la instancia de la base de datos.
   } catch (err) {
     console.error(err.message);
     process.exit(1);
